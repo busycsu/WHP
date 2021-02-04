00bfa178d94316b079753b889eb12c6bb4f3d5d1
csu <imcssu@gmail.com>
2021-02-04 15:47:43 -0800

    no aws

--- WHP_frontend/src/components/aws/aws-credential.jsx
+++ WHP_frontend/src/components/aws/aws-credential.jsx
@@ -1,5 +1,5 @@
 export const credential = {
     region: 'us-west-2', 
-    accessKeyId: 'AKIAIB43YYZBYB4HVKCQ', 
-    secretAccessKey: '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'
+    accessKeyId: REACT_AWS_ACCESS_KEY_ID, 
+    secretAccessKey: REACT_AWS_SECRET_ACCESS_KEY
 }
